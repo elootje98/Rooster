f8c39508b3a5ae031009dc837446bf9608779e49
wesselrijk <wesselrijk@gmail.com>
2019-05-09 19:32:20 +0200

    Initial hillclimber, finds random lectures to swap

--- main.py
+++ main.py
@@ -1,31 +1,22 @@
 from data import data as d
 import numpy as np
 import objective as o
-import timetable as t
+import timetable_test as t
 import printer as p
 from algorithms import random as a
-<<<<<<< HEAD
 from algorithms import hillclimber as hill
-=======
 from algorithms import greedy as g
->>>>>>> 8e6fcca98c84c3ae042f6bd25c28e780155bcc7b
 
-timetable = t.Timetable(d.courses, d.lectures, d.classrooms)
-timetable.make_children()
-timetable.assign_children()
-timetable.sort()
+timetable = t.Timetable()
 
 #a.make_grid(timetable)
 
-g.give_points_lectures()
+#g.give_points_lectures()
 # print(timetable.grid)
 # o.objective_function(timetable)
 
+a.make_grid(timetable)
 
-
-<<<<<<< HEAD
 p.make_table(timetable)
 hill.hillclimber(timetable)
-=======
 #p.make_table(timetable)
->>>>>>> 8e6fcca98c84c3ae042f6bd25c28e780155bcc7b
--- main_test.py
+++ main_test.py
@@ -4,11 +4,8 @@ import timetable_test as t
 import objective_test as o
 from data import data_test as d
 from algorithms import random_test as random
-<<<<<<< HEAD
-from algorithms import hillclimber as hill
-=======
 from algorithms import greedy as greedy
->>>>>>> 8e6fcca98c84c3ae042f6bd25c28e780155bcc7b
+from algorithms import hillclimber as hill
 
 # if len(sys.argv) != 2:
 #     print("Please provide an algorithm. Correct usage: main.py random.")
@@ -24,10 +21,13 @@ timetable = t.Timetable()
 # if algorithm == "greedy":
 
 
-greedy.make_table(timetable)
+#greedy.make_table(timetable)
 
+random.make_table(timetable)
 hill.hillclimber(timetable)
-print(o.objective_function(timetable))
+
+
+# print(o.objective_function(timetable))
 
 
 ### Prints out all lectures made in ID order
