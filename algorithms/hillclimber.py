c7295da5da144c38b35fe79554874e5df9e8dbe9
wesselrijk <wesselrijk@gmail.com>
2019-05-16 18:36:06 +0200

    Thuis verder werken op pc

--- algorithms/hillclimber.py
+++ algorithms/hillclimber.py
@@ -5,33 +5,21 @@ import numpy as np
 import objective as o
 from classes import empty
 
-def hillclimber(timetable, iterations, *args):
-    """Algorithm: iterating over a premade timetable, swaps two random lectures"""
 
-    points_timetable = o.objective_function(timetable)
+def print_hello():
+    print("Hello World!")
 
-    # iterates over a range
-    for i in range(iterations):
-    # while (points_timetable < -100): # use if you want to iterate to a score
 
-        # swap two random lectures
-        lecture_1 = random_lecture(timetable)
-        while lecture_1[0] == empty.Empty:
-            lecture_1 = random_lecture(timetable)
-        lecture_2 = random_lecture(timetable)
-        swap_lectures(timetable, lecture_1, lecture_2)
-        after_points = o.objective_function(timetable)
+def greedy_hill(timetable, samples = 200, chance = 0.1):
+    if np.random.random_sample() < chance:
+        # number of lectures to be swapped
+        to_swap = []
+        for i in range(2 * samples):
+            to_swap.append(random_lecture(timetable))
+        for j in range(0, len(to_swap), 2):
+            # get points before and after iteration
+            swap_lectures(timetable, to_swap[j], to_swap[j+1])
 
-        # swaps back if the number of points decreases
-        if after_points < points_timetable:
-            swap_lectures(timetable, lecture_1, lecture_2)
-
-        after_points = o.objective_function(timetable)
-        points_timetable = o.objective_function(timetable)
-
-        # execute additional functions
-        for functions in args:
-            functions()
 
 def random_lecture(timetable):
     """Returns a random lecture from the timetable grid"""
@@ -49,5 +37,32 @@ def swap_lectures(timetable, lecture_1, lecture_2):
     c2 = lecture_2[1:4]
     timetable.grid[c1[0]][c1[1]][c1[2]], timetable.grid[c2[0]][c2[1]][c2[2]] = timetable.grid[c2[0]][c2[1]][c2[2]], timetable.grid[c1[0]][c1[1]][c1[2]]
 
-def print_hello():
-    print("Hello World!")
+def hill_climbing(timetable):
+    # swap two random lectures
+    lecture_1 = random_lecture(timetable)
+    while lecture_1[0] == empty.Empty:
+        lecture_1 = random_lecture(timetable)
+    lecture_2 = random_lecture(timetable)
+    swap_lectures(timetable, lecture_1, lecture_2)
+    after_points = o.objective_function(timetable)
+
+    # swaps back if the number of points decreases
+    if after_points < points_timetable:
+        swap_lectures(timetable, lecture_1, lecture_2)
+    after_points = o.objective_function(timetable)
+    points_timetable = o.objective_function(timetable)
+
+
+def hillclimber(timetable, iterations, *args):
+    """Algorithm: iterating over a premade timetable, swaps two random lectures"""
+
+    points_timetable = o.objective_function(timetable)
+    functions = {'print_hello': print_hello, 'greedy': greedy_hill}
+
+    # iterates over a range
+    for i in range(iterations):
+    # while (points_timetable < -100): # use if you want to iterate to a score
+        hill_climbing(timetable)
+        # execute additional functions, TODO: does not work
+        for function in args:
+            functions[function]()
