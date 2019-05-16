a0aef0b01374243b795dbcd3279bfc1abb0eb5f5
wesselrijk <wesselrijk@gmail.com>
2019-05-16 20:35:13 +0200

    Implemented greedy_hill function

--- algorithms/hillclimber.py
+++ algorithms/hillclimber.py
@@ -40,32 +40,40 @@ def hill_climbing(timetable, points_timetable):
         swap_lectures(timetable, lecture_1, lecture_2)
 
 
-def greedy_hill(timetable, samples = 50, chance = 0.1):
+def greedy_hill(timetable, points_timetable, samples = 50, chance = 0.1):
     if np.random.random_sample() < chance:
-        print("yo")
-        # number of lectures to be swapped
-        to_swap = []
-        scores = []
-        for i in range(2 * samples):
+        to_swap = [] # number of lectures to be swapped
+        scores = [] # keep track of scores
+        for i in range(2*samples):
             to_swap.append(random_lecture(timetable))
         for j in range(0, len(to_swap), 2):
-            # get points before and after iteration
+            # swap lectures, check score, swap back
             swap_lectures(timetable, to_swap[j], to_swap[j+1])
             scores.append(o.objective_function(timetable))
+            swap_lectures(timetable, to_swap[j], to_swap[j+1])
+        max_score = scores.index(max(scores))
+        swap_lectures(timetable, to_swap[2*max_score], to_swap[2*max_score+1])
 
 
 def hillclimber(timetable, iterations, *args):
     """Algorithm: iterating over a premade timetable, swaps two random lectures"""
 
     points_timetable = o.objective_function(timetable)
-    functions = {'print_hello': print_hello, 'greedy': greedy_hill}
+    functions = {'print_hello': print_hello, 'greedy_hill': greedy_hill}
 
     # iterates over a range
     for i in range(iterations):
     # while (points_timetable < -100): # use if you want to iterate to a score
         hill_climbing(timetable, points_timetable)
-        points_timetable = o.objective_function(timetable)
 
         # apply optional added functions
         for function in args:
-            functions[function]()
+            try:
+                try:
+                    functions[function]()
+                except TypeError:
+                    functions[function](timetable, points_timetable)
+            except KeyError:
+                print(function + " is not a valid function.")
+                return
+        points_timetable = o.objective_function(timetable)
