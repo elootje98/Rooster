445981dacb8adf68334313db80116334c5230bd3
wesselrijk <wesselrijk@gmail.com>
2019-05-24 15:42:10 +0200

    Don't be a pushy

--- helpers/printer.py
+++ helpers/printer.py
@@ -1,99 +1,39 @@
-import colorsys
-
-import matplotlib.gridspec as gridspec
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-import seaborn as sns
-from IPython.display import display, HTML
-import ipywidgets as widgets
-
-
-
-cm = sns.light_palette("red", as_cmap=True)
 
 def make_table(timetable):
-    """ UNDER CONSTRUCTION """
-    # fig = plt.figure()
-    #gs = gridspec.GridSpec(nrows=4, ncols=2)
+    """ Function to print the timetable to readable output and write it to a
+    file.
+
+    The function uses pandas to create dataframes that, converted from the
+    numpy NDarray, are easy to print as well as write to a file. The resulting
+    file will be stored in the "Rooster/results" as a .txt file.
+
+    Arguments:
+        timetable (Timetable): The timetable to be printed.
+
+    """
+
     f = open('./results/timetable.txt' ,'w')
     i = 0
+
+    # Iterate over classrooms to print and write timetables.
     for classroom in timetable.classrooms:
-     #classroom in range(len(d.classrooms)):
-        ### test with matplotlib
-        #ax = fig.add_subplot(gs[int(classroom/2) , classroom%2])
-        #ax.set_title(d.classrooms[classroom])
-        #plt.imshow(np.transpose(timetable.grid[classroom]))
-
-        min_score = 100
-        max_score = -100
-
-        current_scores = np.zeros((5,5), dtype=float)
-        for days in range(5):
-            for slots in range(5):
-                current_scores[days][slots] = timetable.grid[i][days][slots].score
-                if timetable.grid[i][days][slots].course != "empty":
-                    if timetable.grid[i][days][slots].score < min_score:
-                        min_score = timetable.grid[i][days][slots].score
-                    elif timetable.grid[i][days][slots].score > max_score:
-                        max_score =  timetable.grid[i][days][slots].score
-        score_range = float(max_score - min_score)
-
-        rgb = colorsys.hsv_to_rgb(i / score_range, 1.0, 1.0)
-
-
-        # using dataframes for a simple output
         print("\n\n----- " + classroom.name + " -----")
         f.write("\n\n----- " + classroom.name + " -----\n")
 
-
+        # Using pandas dataframes for a simple output.
         columns = ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5']
         index = ['9:00', '11:00', '13:00' , '15:00', '17:00']
         df = pd.DataFrame(np.transpose(timetable.grid[i]), columns, index)
 
-        # change values in the dataframe to the course and type
+        # Change values in the dataframe to the course and type.
         for courses in timetable.courses:
             for lecture in courses.lectures:
                 df = df.replace(lecture, lecture.type + ": " + lecture.course)
-                #df = df.replace(lecture, lecture.score)
-        #with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-        #    print(df)
 
-
-        df.style
-        # display(df)
-        # display(HTML(df.to_html()))
-        # widget1 = widgets.Output()
-        #
-        # with widget1:
-        #     display.display(df)
-        # hbox = widgets.HBox(widget1)
-        # hbox
-
-
-        print(df.to_html())
-        fig, ax = plt.subplots()
-        im = ax.imshow(current_scores)
-        # We want to show all ticks...
-        ax.set_xticks(np.arange(len(columns)))
-        ax.set_yticks(np.arange(len(index)))
-        # ... and label them with the respective list entries
-        ax.set_xticklabels(columns)
-        ax.set_yticklabels(index)
-
-
-        # Loop over data dimensions and create text annotations.
-        for days in range(5):
-            for slots in range(5):
-                text = ax.text(days, slots, timetable.grid[i][days][slots].course,
-                               ha="center", va="center", color="w")
-
-
-        with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
-            print(df)
+        print(df.to_string())
         f.write(df.to_string())
         i += 1
     f.close()
-    #fig.tight_layout()
-    #plt.show()
