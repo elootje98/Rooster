72dbc16987507d40cb5058597bb8206189d962ec
elootje98 <elodierijnja@hotmail.nl>
2019-05-24 11:11:12 +0200

    h

--- algorithms/greedy.py
+++ algorithms/greedy.py
@@ -9,7 +9,20 @@ from helpers import timetable.helpers as hlp
 
 
 def make_table(timetable):
-    """ Makes the timetable, with the course sorted on their points. """
+    """Makes timetable using greedy
+
+    First calls gives_points_lectures to sort the courses based on their
+    points. The lectures of courses with the highest points, will be planned
+    in first. After planning in the lecture, there is checked if the lectures
+    of that course are in the right order. If they are not, the lectures will
+    be removed and a new attampt will be made. In the case of >1000 attemps
+    (which most likely mean that the algorithm is stuck in a loop), a complete
+    new timetable will be made.
+
+    Returns:
+        timetable.
+
+    """
 
     # First sort the courses based on points
     give_points_lectures(timetable)
@@ -34,7 +47,14 @@ def make_table(timetable):
 
 
 def give_points_lectures(timetable):
-    """ Sort the courses based on their 'difficulty' for planning them in. """
+    """Sort the courses based on their 'difficulty' for planning them in.
+
+    Loops over the courses and gives points to the course, based on how
+    difficult they are to plan in. For each course that can't be given at the
+    same time, 20 points are given. Furthermore, for each HC, 10 points are
+    given to the course.
+
+    """
 
     # Loop over the courses and lectures
     for course in timetable.courses:
